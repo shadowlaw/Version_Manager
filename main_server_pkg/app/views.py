19079f2253e462b0ff32383731e8a17be5fcc44d
Javed Wright <jdwright870@gmail.com>
2018-02-17 22:33:18 +0000

    edited requirement

--- main_server_pkg/app/views.py
+++ main_server_pkg/app/views.py
@@ -1,6 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+
 @app.route('/login')
 def login():
     pass
