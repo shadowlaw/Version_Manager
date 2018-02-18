485f05870158ba4c907cc6736215a4d34e89a13a
Javed Wright <jdwright870@gmail.com>
2018-02-18 18:47:16 -0500

    form bug fixes

--- main_server_pkg/app/views.py
+++ main_server_pkg/app/views.py
@@ -1,11 +1,17 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from forms import LoginForm
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-    
+	return render_template('home.html')
 
-@app.route('/login')
+
+@app.route('/login', methods=["GET","POST"])
 def login():
-    pass
+	loginForm = LoginForm()
+
+	if request.method == 'POST' and loginForm.validate_on_submit():		
+		return redirect(url_for('home'))
+
+	return render_template("login.html", loginForm = loginForm)
